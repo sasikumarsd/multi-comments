3a0e849ab1940321227ec03947a6f2e18bfc1a6b
sasikumarsd <sasikr2000@gmail.com>
2025-07-10 14:11:37 +0530

    Final Livewire v3 setup with correct HTTPS loading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
     <title>Multi-Level Comments</title>
+    
+    {{-- Bootstrap CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    {{-- Vite (handles Livewire too) --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    {{-- Livewire Styles --}}
     @livewireStyles
 </head>
 <body class="bg-light">
+
     <div class="container py-4">
         @yield('content')
     </div>
-    @livewireScripts(['asset_url' => 'https://multi-comments-production.up.railway.app'])
+
+    {{-- Livewire Scripts --}}
+    @livewireScripts
 </body>
 </html>
