997191a6ce64993fd7f854cf5e589305e0d0a241
sasikumarsd <sasikr2000@gmail.com>
2025-07-10 15:16:47 +0530

    Rebuild Vite assets with proper HTTPS config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: process.env.APP_URL + '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
